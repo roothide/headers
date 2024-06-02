1644589cc7beadd0fa7dfcd9308762ea801dfb1e
roothider <158083651+roothider@users.noreply.github.com>
2024-06-02 13:36:01 +0800

    fix for compile-test

--- roothide/stub.h
+++ roothide/stub.h
@@ -1,17 +1,15 @@
 #ifndef ROOTHIDE_H
 #define ROOTHIDE_H
 
-#if defined(THEOS_PACKAGE_SCHEME_ROOTFUL)
-#define __ROOT_PATH_VAR(x)  x
-#define __ROOT_PATH_NS_VAR(x)   x
-#pragma message("building for rootful, fall back to stub functions")
-#elif defined(THEOS_PACKAGE_SCHEME_ROOTLESS)
-#include <rootless.h>
-#define __ROOT_PATH_VAR(x) ROOT_PATH_VAR(x)
-#define __ROOT_PATH_NS_VAR(x) ROOT_PATH_NS_VAR(x)
-#pragma message("building for rootless, fall back to rooless.h")
+#ifdef THEOS_PACKAGE_SCHEME_ROOTHIDE
+#define __C_STRING_PATH_FALLBACK(x)  x
+#define __NS_STRING_PATH_FALLBACK(x)   x
+// #pragma message("building for roothide")
 #else
-#pragma error("unknown package scheme")
+#include <rootless.h>
+#define __C_STRING_PATH_FALLBACK(x) ROOT_PATH_VAR(x)
+#define __NS_STRING_PATH_FALLBACK(x) ROOT_PATH_NS_VAR(x)
+// #pragma message("fall back to rootless.h")
 #endif
 
 #pragma GCC diagnostic push
@@ -35,15 +33,15 @@ extern "C" {
 #endif
 
 static const char* rootfs_alloc(const char* path) { return path ? strdup(path) : path; }
-static const char* jbroot_alloc(const char* path) { return path ? strdup(__ROOT_PATH_VAR(path)) : path; }
-static const char* jbrootat_alloc(int fd, const char* path) { return path ? strdup(__ROOT_PATH_VAR(path)) : path; }
+static const char* jbroot_alloc(const char* path) { return path ? strdup(__C_STRING_PATH_FALLBACK(path)) : path; }
+static const char* jbrootat_alloc(int fd, const char* path) { return path ? strdup(__C_STRING_PATH_FALLBACK(path)) : path; }
 
 static unsigned long long jbrand() { return 0; }
-static const char* jbroot(const char* path) { return __ROOT_PATH_VAR(path); }
+static const char* jbroot(const char* path) { return __C_STRING_PATH_FALLBACK(path); }
 static const char* rootfs(const char* path) { return path; }
 
 #ifdef __OBJC__
-static NSString* _Nonnull __attribute__((overloadable)) jbroot(NSString* _Nonnull path) { return __ROOT_PATH_NS_VAR(path); }
+static NSString* _Nonnull __attribute__((overloadable)) jbroot(NSString* _Nonnull path) { return __NS_STRING_PATH_FALLBACK(path); }
 static NSString* _Nonnull __attribute__((overloadable)) rootfs(NSString* _Nonnull path) { return path; }
 #endif
 
@@ -52,7 +50,7 @@ static NSString* _Nonnull __attribute__((overloadable)) rootfs(NSString* _Nonnul
 #endif
 
 #ifdef __cplusplus
-static std::string jbroot(std::string path) { return std::string(__ROOT_PATH_VAR(path.c_str())); }
+static std::string jbroot(std::string path) { return std::string(__C_STRING_PATH_FALLBACK(path.c_str())); }
 static std::string rootfs(std::string path) { return path; }
 #endif
 
