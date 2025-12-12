761b68840395d5bce91b6502b78c9df2f81b4137
Leptos <leptos.0.null@gmail.com>
2025-12-11 23:11:44 -0800

    [DoNotDisturb] Adjust NSError out parameter

--- DoNotDisturb/DNDStateService.h
+++ DoNotDisturb/DNDStateService.h
@@ -5,6 +5,6 @@ API_AVAILABLE(ios(12.0))
 
 + (instancetype)serviceForClientIdentifier:(NSString *)identifier;
 
-- (DNDState *)queryCurrentStateWithError:(NSError *)error;
+- (DNDState *)queryCurrentStateWithError:(NSError **)error;
 
 @end
