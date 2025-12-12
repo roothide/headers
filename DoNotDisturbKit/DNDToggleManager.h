df80b2d5f5ca6a03c51e418547b3a9db487a7183
Leptos <leptos.0.null@gmail.com>
2025-12-11 23:12:10 -0800

    [DoNotDisturbKit] Adjust NSError out parameter

--- DoNotDisturbKit/DNDToggleManager.h
+++ DoNotDisturbKit/DNDToggleManager.h
@@ -5,7 +5,7 @@ API_AVAILABLE(ios(14.0))
 
 + (instancetype)managerForClientIdentifier:(NSString *)identifier;
 
-- (BOOL)_toggleDNDOffReturningError:(NSError *)error;
-- (BOOL)_toggleDNDOnReturningError:(NSError *)error;
+- (BOOL)_toggleDNDOffReturningError:(NSError **)error;
+- (BOOL)_toggleDNDOnReturningError:(NSError **)error;
 
 @end
