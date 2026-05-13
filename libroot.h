1d144c1e0e9fe8495c7fab7d8242a7f0afcb56bf
roothider <158083651+roothider@users.noreply.github.com>
2026-05-13 19:43:07 +0800

    Add rootless.h compatibility to support building roothide packages

--- libroot.h
+++ libroot.h
@@ -1,1 +1,5 @@
+#ifdef THEOS_PACKAGE_SCHEME_ROOTHIDE
+  #error "Please upgrade to the roothide APIs for full compatibility with rootful/rootless/roothide : https://github.com/roothide/Developer"
+#endif
+
 #include <libroot/libroot.h>
--- roothide/stub.h
+++ roothide/stub.h
@@ -1,16 +1,7 @@
 #ifndef ROOTHIDE_H
 #define ROOTHIDE_H
 
-#ifdef THEOS_PACKAGE_SCHEME_ROOTHIDE
-#define __C_STRING_PATH_FALLBACK(x)  x
-#define __NS_STRING_PATH_FALLBACK(x)   x
-// #pragma message("building for roothide")
-#else
 #include <rootless.h>
-#define __C_STRING_PATH_FALLBACK(x) ROOT_PATH_VAR(x)
-#define __NS_STRING_PATH_FALLBACK(x) ROOT_PATH_NS_VAR(x)
-// #pragma message("fall back to rootless.h")
-#endif
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
@@ -33,15 +24,15 @@ extern "C" {
 #endif
 
 static const char* rootfs_alloc(const char* path) { return path ? strdup(path) : path; }
-static const char* jbroot_alloc(const char* path) { return path ? strdup(__C_STRING_PATH_FALLBACK(path)) : path; }
-static const char* jbrootat_alloc(int fd, const char* path) { return path ? strdup(__C_STRING_PATH_FALLBACK(path)) : path; }
+static const char* jbroot_alloc(const char* path) { return path ? strdup(ROOT_PATH_VAR(path)) : path; }
+static const char* jbrootat_alloc(int fd, const char* path) { return path ? strdup(ROOT_PATH_VAR(path)) : path; }
 
 static unsigned long long jbrand() { return 0; }
-static const char* jbroot(const char* path) { return __C_STRING_PATH_FALLBACK(path); }
+static const char* jbroot(const char* path) { return ROOT_PATH_VAR(path); }
 static const char* rootfs(const char* path) { return path; }
 
 #ifdef __OBJC__
-static NSString* _Nonnull __attribute__((overloadable)) jbroot(NSString* _Nonnull path) { return __NS_STRING_PATH_FALLBACK(path); }
+static NSString* _Nonnull __attribute__((overloadable)) jbroot(NSString* _Nonnull path) { return ROOT_PATH_NS_VAR(path); }
 static NSString* _Nonnull __attribute__((overloadable)) rootfs(NSString* _Nonnull path) { return path; }
 #endif
 
@@ -50,10 +41,37 @@ static NSString* _Nonnull __attribute__((overloadable)) rootfs(NSString* _Nonnul
 #endif
 
 #ifdef __cplusplus
-static std::string jbroot(std::string path) { return std::string(__C_STRING_PATH_FALLBACK(path.c_str())); }
+static std::string jbroot(std::string path) { return std::string(ROOT_PATH_VAR(path.c_str())); }
 static std::string rootfs(std::string path) { return path; }
 #endif
 
 #pragma GCC diagnostic pop
+
+#define __INCOMPATIBLE_MACRO	_Pragma("GCC error \"Please upgrade to the roothide APIs for full compatibility with rootful/rootless/roothide : https://github.com/roothide/Developer\"")
+
+#undef	ROOT_PATH
+#undef	ROOT_PATH_NS
+#undef	ROOT_PATH_VAR
+#undef	ROOT_PATH_NS_VAR
+#undef	JBROOT_PATH
+#undef	JBROOT_PATH_CSTRING
+#undef	JBROOT_PATH_NSSTRING
+#undef	ROOTFS_PATH
+#undef	ROOTFS_PATH_CSTRING
+#undef	ROOTFS_PATH_NSSTRING
+
+#define ROOT_PATH(cPath)				({__INCOMPATIBLE_MACRO; (const char *)0; })
+#define ROOT_PATH_NS(path)				({__INCOMPATIBLE_MACRO; (const char *)0; })
+
+#define ROOT_PATH_VAR(path)				({__INCOMPATIBLE_MACRO; (const char *)0; })
+#define ROOT_PATH_NS_VAR(path)			({__INCOMPATIBLE_MACRO; (const char *)0; })
+
+#define JBROOT_PATH(path)				({__INCOMPATIBLE_MACRO; (const char *)0; })
+#define JBROOT_PATH_CSTRING(path)		({__INCOMPATIBLE_MACRO; (const char *)0; })
+#define JBROOT_PATH_NSSTRING(path)		({__INCOMPATIBLE_MACRO; (const char *)0; })
+
+#define ROOTFS_PATH(path) 				({__INCOMPATIBLE_MACRO; (const char *)0; })
+#define ROOTFS_PATH_CSTRING(path) 		({__INCOMPATIBLE_MACRO; (const char *)0; })
+#define ROOTFS_PATH_NSSTRING(path) 		({__INCOMPATIBLE_MACRO; (const char *)0; })
 
 #endif /* ROOTHIDE_H */
